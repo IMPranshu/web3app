2de35f5abf397d7f43a5b13e5e57fd5830db85f6
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-13 19:26:02 +0530

    react-icons & ethers package added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
+import logo from '../../img/logo.png';
+
 const Navbar = () => {
   return(
-    <h1>NavBar</h1>
+    <nav classname="w-full flex md:justify-center justify-between items-center p-4">
+
+    </nav>
   );
 }
 
